a39f5036bb83bef1927da66b94a9e71622dd29df
dc-p8 <cardinal.dorian@gmail.com>
2018-12-13 18:15:42 +0100

    cleanup repos

--- servo.py
+++ servo.py
@@ -3,50 +3,16 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
-GPIO.setwarnings(False)
+#GPIO.setwarnings(False)
 
-ajoutAngle = 5
+pwm=GPIO.PWM(17,50)
+pwm.start(5)
 
-print("\n+----------/ ServoMoteur  Controlleur /----------+")
-print("|                                                |")
-print("| Le Servo doit etre branche au pin 11 / GPIO 17 |")
-print("|                                                |")
-print("+------------------------------------------------+\n")
+while True:
+	angle = input("Entrez l'angle souhaite (Entre 0 et 100 %):\n")
+	pwm.ChangeDutyCycle(angle)
+	time.sleep(1)
 
-print("Comment controler le Servo ?")
-choix = int(input("1. Choisir un angle\n2. Faire tourner de 0 a 100%\n"))
-
-if choix == 2 :
-
-    nbrTour = input ("Entrez le nombre d'aller-retour que fera le Servo :\n")
-
-    pwm=GPIO.PWM(17,100)
-    pwm.start(5)
-
-    angle1 = 0
-    duty1 = float(angle1)/10 + ajoutAngle
-
-    angle2=180
-    duty2= float(angle2)/10 + ajoutAngle
-
-    i = 0
-
-    while i <= nbrTour:
-         pwm.ChangeDutyCycle(duty1)
-         time.sleep(0.8)
-         pwm.ChangeDutyCycle(duty2)
-         time.sleep(0.8)
-         i = i+1
-    GPIO.cleanup()
-
-if choix == 1 :
-    angle = input("Entrez l'angle souhaite (Entre 0 et 100 %):\n")
-    duree = input("Entrez la duree durant laquelle le Servo devra tenir sa position : ( en secondes )\n")
-
-    pwm=GPIO.PWM(17,100)
-    pwm.start(0)
-
-    #angleChoisi = float(angle)/10 + ajoutAngle
-    pwm.ChangeDutyCycle(angle)
-    time.sleep(duree)
-    GPIO.cleanup()
+except KeyboardInterrupt:
+	pwm.stop()
+	GPIO.cleanup()
